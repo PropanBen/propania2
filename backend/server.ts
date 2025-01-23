264f0afb96e36ef74836834dd58cc5c50aba2a17
PropanBen <propanben@freenet.de>
2025-01-23 22:54:42 +0100

    Layout Login Page

--- backend/server.ts
+++ backend/server.ts
@@ -6,6 +6,7 @@ import cors from 'cors';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import mariadb from 'mariadb';
+import dotenv from 'dotenv';
 
 const app: Application = express();
 const server = createServer(app);
@@ -27,17 +28,18 @@ app.use(
 		credentials: true, // Falls Cookies verwendet werden
 	})
 );
+dotenv.config();
 app.use(express.json());
 
 const PORT = 3001;
-const JWT_SECRET = 'propania2secret';
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const pool = mariadb.createPool({
-	host: 'loclahost',
-	user: 'example',
-	password: 'example', // Ersetze mit deinem DB-Passwort
-	database: 'exampledb',
-	connectionLimit: 5,
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT || '5', 10),
 });
 
 // Benutzerkonto erstellen
@@ -63,11 +65,12 @@ app.post('/register', async (req, res) => {
 });
 
 // Benutzer-Login
-app.post('/login', async (req: Request, res: Response) => {
+app.post('/login', async (req: Request, res: Response): Promise<void> => {
 	const { email, password } = req.body as { email: string; password: string };
 
 	if (!email || !password) {
 		res.status(400).json({ message: 'Bitte alle Felder ausfüllen!' });
+		return;
 	}
 
 	try {
@@ -79,6 +82,7 @@ app.post('/login', async (req: Request, res: Response) => {
 
 			if (rows.length === 0) {
 				res.status(401).json({ message: 'Ungültige Anmeldedaten!' });
+				return;
 			}
 
 			const user = rows[0];
@@ -89,23 +93,26 @@ app.post('/login', async (req: Request, res: Response) => {
 
 			if (!isPasswordValid) {
 				res.status(401).json({ message: 'Ungültige Anmeldedaten!' });
+				return;
 			}
 
 			const token = jwt.sign(
 				{ userId: user.id, email: user.email },
-				JWT_SECRET,
+				JWT_SECRET!,
 				{
 					expiresIn: '1h',
 				}
 			) as string;
 
 			res.status(200).json({ message: 'Login erfolgreich!', token });
+			return;
 		} finally {
 			conn.release();
 		}
 	} catch (error) {
 		console.error('Error during login:', error);
 		res.status(500).json({ message: 'Fehler beim Login' });
+		return;
 	}
 });
 
@@ -127,7 +134,7 @@ function authenticateToken(
 	}
 
 	if (typeof token === 'string') {
-		jwt.verify(token, JWT_SECRET, (err, user) => {
+		jwt.verify(token, JWT_SECRET!, (err, user) => {
 			if (err) {
 				return res.status(403).json({ message: 'Ungültiger Token!' });
 			}
